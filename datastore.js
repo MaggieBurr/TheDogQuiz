a5fc7c3e18cc82ac8af0be45dea41f1387bd3e1a
MaggieBurr <44220984+MaggieBurr@users.noreply.github.com>
2019-07-16 11:49:19 -0700

    Create datastore.js

---
+++ datastore.js
@@ -0,0 +1,128 @@
+
+const STORE = [
+  {
+    question: 'What part of the body do dogs sweat?',
+    answer: [
+      'They don’t sweat at all',
+      'paws', 
+      'mouth', 
+      'armpit' ],
+    correctAnswer : 'paws'
+    
+  },
+
+  {
+    question:'At what age is a puppy ready to be adopted?',
+    answer: [
+        '3 months',
+        '8 weeks',
+        '6 weeks',
+        '2.5 months',
+    ],
+    correctAnswer : '8 weeks'
+
+  },
+
+{
+    question: "What are the names of President Obama's dogs?",
+    answer: [
+      "Bo and Mo",
+      "The Obamas only have one dog named Bo",
+      "Maggie and Max",
+      "Bo and Sunny",
+    ],
+    correctAnswer : "Bo and Sunny"
+
+  },
+
+   {
+    question:"In the movie Air Bud, what was the dog's name and his breed?",
+    answer: [
+      "Bud, Labrador Retriever",
+      "Buddy, Golden Retriever",
+      "Bud, Golden Retriever"
+    ],
+
+    correctAnswer : "Bud, Labrador Retriever"
+
+  },
+
+   {
+    question: "What are dogs most closely related to?",
+    answer: [
+      "wolves",
+      "bears",
+      "coyotes",
+      "horses",
+    ],
+
+    correctAnswer : "wolves"
+
+  },
+
+   {
+    question: "Which breed of dog is The Queen of England’s favorite?",
+    answer: [
+      "Pomeranian",
+      "Labrador Retriever",
+      "Corgi",
+      "Poodle",
+    ],
+
+    correctAnswer : "Corgi"
+
+  },
+
+   {
+    question: "What is the most popular breed of dog according to the American Kennel Club’s registrations?",
+    answer: [
+      "Retriever",
+      "Beagle",
+      "German Shephard",
+      "Labrador",
+    ],
+
+    correctAnswer : "Retriever"
+
+  },
+
+   {
+    question: "What breed is Scooby Doo?",
+    answer: [
+      "Dalmation",
+      "German Shephard",
+      "Great Dane",
+      "English Mastiff",
+    ],
+
+    correctAnswer : "Great Dane"
+
+  },
+
+   {
+    question: 'Where is the breed "Australian Shepherd" from?',
+    answer: [
+      "Japan",
+      "Australia",
+      "United States",
+      "Canada",
+    ],
+
+    correctAnswer : "United States"
+
+  },
+
+   {
+    question: "What is the largest breed?",
+    answer: [
+      "Great Dane",
+      "Irish Wolfhound",
+      "St. Bernard",
+      "English Mastiff",
+    ],
+
+    correctAnswer : "Irish Wolfhound"
+
+  }
+
+];
